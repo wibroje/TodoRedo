bfd3bb177325eda657e1bab5ecf724b3d741a209
William Jefferies <willjefferies10@gmail.com>
2018-04-18 21:28:37 -0600

    1. Restarted and restructured To-Do List

---
+++ models/todo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let todoSchema = new Schema({
+	description: String,
+	done: Boolean
+});
+
+module.exports = mongoose.model('Todo', todoSchema);
